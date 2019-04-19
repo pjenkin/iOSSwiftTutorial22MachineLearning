dc5435eff05ac8bc26215d4377353aebe9bc31f3
Peter Jenkin <peternjenkin@gmail.com>
2019-04-19 16:20:59 +0100

    22-232 Controls set to allow user to choose image

--- Photo Machine Learning/ViewController.swift
+++ Photo Machine Learning/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView.isUserInteractionEnabled = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +27,53 @@ class ViewController: UIViewController {
     }
 
     @IBAction func changeBtnClicked(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
     }
+    /*
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        var selectedImageFromPicker: UIImage?   // workaround to get original image https://stackoverflow.com/a/53219069
+        
+        // postImage.image = info[UIImagePickerControllerEditedImage] as? UIImage // didn't work as original image slipped through unused - no edited image to use
+        
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage
+        {   // NB Any type from dictionary - try to cast to UIImage
+            selectedImageFromPicker = editedImage
+        }
+        else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            selectedImageFromPicker = originalImage
+        }
+        
+        // cautious approach here!
+        if let selectedImage = selectedImageFromPicker
+        {
+            imageView.image = selectedImage
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+*/
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        // colossal flag name - typed in nearly all before auto-complete
+        // then UIImage auto-completed to huge name!
+        // (happening on video too !)
+        // get (hopefully) Image from picker cast as UIIMage
+        //self.dismiss(animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        // info is a dictionary
+    }
+
+    
+    
     
 }
 
